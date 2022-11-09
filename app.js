6c21b50010d5df8379c315140cd87a1143d1612a
hughpw <hughkwabiah@gmail.com>
2022-11-09 21:41:02 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+
+/*
+const exArray = [
+  [1,2,3,4],
+  ["a","b","c"],
+  [true,false,null]
+];
+
+for (let i of exArray) {
+  for (let j of i) {
+    console.log(j)
+  };
+  //console.log(i)
+};
+*/
+
+/*
+const shoeSize = {
+  Jordan: 13,
+  Hugh: 8,
+  Jadon: 5,
+  Mum: 7,
+  Dad: 9
+};
+
+for (let i in shoeSize) {
+  console.log(`${i}'s shoe size is ${shoeSize[i]}.`);
+}
+*/
+
+/*
+const arrayOfCountries = [
+  {Country: "Ghana", Population: 30},
+  {Country: "England", Population: 56},
+  {Country: "Brazil", Population: 213},
+  {Country: "US", Population: 330},
+];
+
+console.log(arrayOfCountries);
+
+arrayOfCountries.map((a) => {
+  console.log(`${a.Country} has a population of ${
+    (a.Population * 1000000).toLocaleString("en-US")}.`);
+});
+*/
